36c28b06e2ed5f1c6cf13847aa0c456f60b25cc3
Anneloes <anneloes9405@hotmail.com>
2020-01-29 20:16:17 +0000

    rendering tweetdata modularly now works

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  // returns the time between a given date and current dat
   const timeBetween = (date) => {
     const msPerDay = 1000 * 60 * 60 * 24
     let currentDate = new Date()
@@ -9,63 +9,61 @@ $(document).ready(function() {
     } else if (diffInTime >= 1) {
       return Math.floor(diffInTime) + " days ago"
     } else {
-      return Math.floor(diffInTime*24) + " hours ago"
+      return Math.floor(diffInTime * 24) + " hours ago"
     }
   }
 
-  console.log(new Date())
+  // creates one tweet element
+  const createTweetElement = (tweet) => {
+    const {
+      name,
+      avatars,
+      handle,
+    } = tweet.user
+    const { text } = tweet.content
+    const { created_at } = tweet
+  
+    const tweetHTML = (`
+    <article class="tweet">
+      <header>
+        <img src=${avatars} alt="avatar">
+        ${name}
+        <span>${handle}</span>
+      </header>
+      <p>
+        ${text}
+      </p>
+      <footer>
+        ${timeBetween(new Date(created_at))}
+      </footer>
+    `)
+    return tweetHTML
+  }
 
-  const createTweetElement = (tweetData) => {
+
+  const renderTweets = function(tweets) {
     const tweetArray = []
-    for (tweet of tweetData) {
-      const {
-        name,
-        avatars,
-        handle,
-      } = tweet.user
-
-      const { text } = tweet.content
-
-      const { created_at } = tweet
-    
-
-      tweetArray.push(`
-      <article class="tweet">
-        <header>
-          ${name}
-          ${avatars}
-          ${handle}
-        </header>
-        <p>
-          ${text}
-        </p>
-        <footer>
-          ${timeBetween(new Date(created_at))}
-        </footer>
-      `)
+    for (tweet of tweets) {
+      const tweetElement = createTweetElement(tweet)
+      tweetArray.push(tweetElement)
     }
-    return tweetArray;
+    
+    $('main #tweetSection').append(tweetArray.join(''))
   }
 
     // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [{
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-          "handle": "@SirIsaac"
-        },
-      "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-      "created_at": 1461116232227
-   }]
+  // const tweetData = [{
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //         "handle": "@SirIsaac"
+  //       },
+  //     "content": {
+  //         "text": "If I have seen further it is by standing on the shoulders of giants"
+  //       },
+  //     "created_at": 1461116232227
+  //  }]
   
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('main #tweetSection').append($tweet); // to add it to the page so we can make sure it's   got all the right elements, classes, etc.
-
-
+  renderTweets(data);
+})
       // $('main #tweetSection').append($article);
-  })
