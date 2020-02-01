8d9458744523c93d0550047219a8f1e85cc92b8b
Anneloes <anneloes9405@hotmail.com>
2020-02-01 23:53:59 +0000

    refactoring done

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,24 +4,25 @@ $(document).ready(function() {
     $(`.new-tweet`).slideToggle('fast', function() {
       $(`#tweetContent`).focus();
     })
-    event.stopPropagation()
+    event.stopPropagation();
   })
 
-  $('.error-message').hide()
+  $('.error-message').hide();
 
   // returns the time between a given date and current date
   const timeBetween = (date) => {
-    const msPerDay = 1000 * 60 * 60 * 24
-    let currentDate = new Date()
-    const diffInTime = (currentDate - date) / msPerDay
+    const msPerDay = 1000 * 60 * 60 * 24;
+    let currentDate = new Date();
+    const diffInTime = (currentDate - date) / msPerDay;
+
     if (diffInTime >= 365) {
-      return Math.floor(diffInTime / 365) + " years ago"
+      return Math.floor(diffInTime / 365) + " years ago";
     } else if (diffInTime >= 1) {
-      return Math.floor(diffInTime) + " days ago"
+      return Math.floor(diffInTime) + " days ago";
     } else {
-      return Math.floor(diffInTime * 24) + " hours ago"
-    }
-  }
+      return Math.floor(diffInTime * 24) + " hours ago";
+    };
+  };
 
   // creates one tweet element
   const createTweetElement = (tweet) => {
@@ -29,41 +30,41 @@ $(document).ready(function() {
       name,
       avatars,
       handle,
-    } = tweet.user
-    const { text } = tweet.content
-    const { created_at } = tweet
+    } = tweet.user;
+    const { text } = tweet.content;
+    const { created_at } = tweet;
 
-    $footerText = timeBetween(new Date(created_at))
+    $footerText = timeBetween(new Date(created_at));
 
-    $like = 'images/Like.png'
-    $retweet = 'images/Retweet.png'
-    $flag = 'images/Flag.png'
+    $like = 'images/Like.png';
+    $retweet = 'images/Retweet.png';
+    $flag = 'images/Flag.png';
 
-    $p = $("<p>").text(text)
+    $p = $("<p>").text(text);
     $footerDiv1 = $('<div id="time">').html($footerText);
-    $footerDiv2 = $('<div id="social-links">').html(`<img src=` + $flag + ` alt="flag">` + `<img src=` + $retweet + ` alt="retweet">` + `<img src=` + $like + ` alt="like">`)
-    $footer = $("<footer>").append($footerDiv1, $footerDiv2)
+    $footerDiv2 = $('<div id="social-links">').html(`<img src=` + $flag + ` alt="flag">` + `<img src=` + $retweet + ` alt="retweet">` + `<img src=` + $like + ` alt="like">`);
+    $footer = $("<footer>").append($footerDiv1, $footerDiv2);
     $header = $("<header>").html(`<img src=${avatars} alt="avatar">
-    ${name}<span id="username">${handle}</span>`)
-    $article = $("<article>")
-    $article.addClass("tweet")
+    ${name}<span id="username">${handle}</span>`);
+    $article = $("<article>");
+    $article.addClass("tweet");
 
 
-    $article.append($header,$p,$footer)
+    $article.append($header,$p,$footer);
 
-    return '<article class="tweet">' + $article.html() + '</article>'
+    return '<article class="tweet">' + $article.html() + '</article>';
   }
   
  // takes in an array of tweet objects, stringifies and renders them 
   // aka makes presentable html sections out of tweet objects
   const renderTweets = function(tweets) {
-    const tweetArray = []
+    const tweetArray = [];
     for (tweet of tweets) {
-      const tweetElement = createTweetElement(tweet)
-      tweetArray.unshift(tweetElement)
+      const tweetElement = createTweetElement(tweet);
+      tweetArray.unshift(tweetElement);
     }
 
-    $('main #tweet-section').append(tweetArray.join(''))
+    $('main #tweet-section').append(tweetArray.join(''));
   }
 
  // fetches tweets from /tweets page
@@ -72,12 +73,13 @@ $(document).ready(function() {
       type: 'GET',
       url: '/tweets',
       success: function (data) {
-        renderTweets(data)}
-    })
-  }
+        renderTweets(data);
+      }
+    });
+  };
   
  //render new tweets
-  loadTweets()
+  loadTweets();
 
   //listen for 'submit'
   $('.container').find('form').on('submit', function(event) {
@@ -89,22 +91,23 @@ $(document).ready(function() {
     console.log('Form submitted, performing ajax call...');
     const maxLength = 140;
     if ($tweet === "") {
-      $(".error-message").html("<strong>Warning! </strong>Your tweet currently has no content")
-      $('.error-message').show()
+      $(".error-message").html("<strong>Warning! </strong>Your tweet currently has no content");
+      $('.error-message').show();
     } else if ($tweet.length > maxLength) {
-      jQuery.fx.off = true
-      $(".error-message").html("<strong>Warning! </strong>Your tweet exceeds the maximum amount of characters")
-      $('.error-message').show()
+      jQuery.fx.off = true;
+      $(".error-message").html("<strong>Warning! </strong>Your tweet exceeds the maximum amount of characters");
+      $('.error-message').show();
     } else {
-      $('.error-message').hide()
+      $('.error-message').hide();
       $.ajax({
         type: 'POST',
         url: '/tweets', 
         "data": $textbox,
         success: function() {
-          $('main #tweet-section').html("")    // clear all html
-          loadTweets()}                       // load tweets again
-      })
-    }
-  })
-})
+          $('main #tweet-section').html("");    // clear all html
+          loadTweets();                       // load tweets again
+        }
+      });
+    };
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
 
   //counts characters
   $('#tweetContent').keyup(function() {
-    let length = $(this).val().length
-    const maxLength = 140
-    let $countTag = $(this).siblings(".counter")
+    let length = $(this).val().length;
+    const maxLength = 140;
+    let $countTag = $(this).siblings(".counter");
 
     if (length <= maxLength) {
       $countTag.html(length);
-      $countTag.css("color", "black")
+      $countTag.css("color", "black");
     } else if (length > maxLength) {
       $countTag.html(maxLength - length);
       $countTag.css("color", "red");
     }
-  })
+  });
 
 });
